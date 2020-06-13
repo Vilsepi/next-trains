71472d0b7c719e436195146e3237b5fe4d8b0cb7
Ville Seppanen <ville.seppanen@polarsquad.com>
2020-06-13 12:13:40 +0300

    Pass stations as url params. More comfortable desktop view.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
 
-moment.updateLocale('en', {
-    relativeTime: {
-        future : '%s',
-        past   : '<span class="smaller">lähti</span>',
-        s: function (number, withoutSuffix) {
-        return '<span class="smaller">nyt</span>';
-        },
-        m  : '1<span class="smaller"> min</span>',
-        mm : '%d<span class="smaller"> min</span>',
-        h  : '1<span class="smaller"> h</span>',
-        hh : '%d<span class="smaller"> h</span>',
-        d  : '1 pv',
-        dd : '%d pv',
-        M  : '1 kk',
-        MM : '%d kk',
-        y  : '1 v',
-        yy : '%d v'
-    }
-});
+function modifyMomentLocale() {
+    moment.updateLocale('en', {
+        relativeTime: {
+            future : '%s',
+            past   : '<span class="smaller">lähti</span>',
+            s: function (number, withoutSuffix) {
+            return '<span class="smaller">nyt</span>';
+            },
+            m  : '1<span class="smaller"> min</span>',
+            mm : '%d<span class="smaller"> min</span>',
+            h  : '1<span class="smaller"> h</span>',
+            hh : '%d<span class="smaller"> h</span>',
+            d  : '1 pv',
+            dd : '%d pv',
+            M  : '1 kk',
+            MM : '%d kk',
+            y  : '1 v',
+            yy : '%d v'
+        }
+    });
+}
 
 function getJson(url, callback) {
     const Http = new XMLHttpRequest();
@@ -62,48 +64,69 @@ function renderTableRow(train, trainListElement) {
     trainListElement.appendChild(rowElement);
 }
 
-const fromStationCode = "TKL";
-const toStationCode = "HKI";
-const maxDepartingTrains = "30";
-const url = `https://rata.digitraffic.fi/api/v1/live-trains/station/${fromStationCode}?arrived_trains=0&arriving_trains=0&departed_trains=0&departing_trains=${maxDepartingTrains}&include_nonstopping=false`;
+function main() {
+    modifyMomentLocale();
+    const stationHeadingElement = document.getElementById("stationHeading");
 
-getJson(url, (response) => {
-    console.debug(JSON.stringify(response, null, 2));
-    // Only show commuter trains that are traveling from fromStationCode to toStationCode
-    // TODO To show only upcoming trains, we filter by liveEstimateTime. This has a side-effect that trains that do not have liveEstimate are not shown at all.
-    // We should instead compare timestamps against now time (or check that actualTime must not be present), and show either liveEstimateTime or scheduledTime.
-    const trains = response.filter(train =>
-        ["Commuter", "Long-distance"].includes(train.trainCategory) && (
-            train.timeTableRows = train.timeTableRows.filter(
-                station =>
-                    (station.stationShortCode == fromStationCode && station.type == "DEPARTURE" && station.liveEstimateTime) ||
-                    (station.stationShortCode == toStationCode && station.type == "ARRIVAL" && station.liveEstimateTime)
-            )
-        ).length > 1);
+    const urlParams = new URLSearchParams(window.location.search);
+    const fromStationCode = urlParams.get('from');
+    const toStationCode = urlParams.get('to');
 
-    // Sort trains by destination arrival time
-    trains.sort(function(a, b) {
-        return moment(a.timeTableRows[1].liveEstimateTime) - moment(b.timeTableRows[1].liveEstimateTime);
-    });
-
-    const trainListElement = document.getElementById("trainList");
-    renderTableHeader(trainListElement);
-
-    for (var i in trains) {
-        const train = trains[i];
-
-        // After filtering in previous stage, each train should have exactly 2 stops, the from and to stations the user is interested in.
-        if (train.timeTableRows.length != 2) {
-            console.error(`Train ${train.trainNumber} has ${train.timeTableRows.length} interesting stops.`);
-            console.log(JSON.stringify(train));
-        }
-        // From and to stations should be in travel order, according to API specification.
-        else if (train.timeTableRows[0].stationShortCode != fromStationCode || train.timeTableRows[1].stationShortCode != toStationCode) {
-            console.error(`Train ${train.trainNumber} has interesting stops in unexpected order.`);
-            console.log(JSON.stringify(train));
-        }
-        else {
-            renderTableRow(train, trainListElement);
-        }
+    if (!fromStationCode || !toStationCode) {
+        console.error("From and to query parameters not given");
+        stationHeadingElement.innerHTML = `Syötä queryparametrit oikein!`;
+        return;
     }
-});
+
+    stationHeadingElement.innerHTML = `${fromStationCode} &#129030; ${toStationCode}`;
+
+    //const fromStationCode = "TKL";
+    //const toStationCode = "HKI";
+    const maxDepartingTrains = "30";
+    const url = `https://rata.digitraffic.fi/api/v1/live-trains/station/${fromStationCode}?arrived_trains=0&arriving_trains=0&departed_trains=0&departing_trains=${maxDepartingTrains}&include_nonstopping=false`;
+
+    getJson(url, (response) => {
+        //console.debug(JSON.stringify(response, null, 2));
+
+        // Only show commuter trains that are traveling from fromStationCode to toStationCode
+        // TODO To show only upcoming trains, we filter by liveEstimateTime. This has a side-effect that trains that do not have liveEstimate are not shown at all.
+        // We should instead compare timestamps against now time (or check that actualTime must not be present), and show either liveEstimateTime or scheduledTime.
+        // TODO When showing scheduledTimes, you must then filter out cancelled trains
+        // FYI train waiting for depart => non-running train
+        const trains = response.filter(train =>
+            ["Commuter", "Long-distance"].includes(train.trainCategory) && (
+                train.timeTableRows = train.timeTableRows.filter(
+                    station =>
+                        (station.stationShortCode == fromStationCode && station.type == "DEPARTURE" && station.liveEstimateTime) ||
+                        (station.stationShortCode == toStationCode && station.type == "ARRIVAL" && station.liveEstimateTime)
+                )
+            ).length > 1);
+
+
+        // Sort trains by destination arrival time
+        trains.sort(function(a, b) {
+            return moment(a.timeTableRows[1].liveEstimateTime) - moment(b.timeTableRows[1].liveEstimateTime);
+        });
+
+        const trainListElement = document.getElementById("trainList");
+        renderTableHeader(trainListElement);
+
+        trains.forEach(train => {
+            // After filtering in previous stage, each train should have exactly 2 stops, the from and to stations the user is interested in.
+            if (train.timeTableRows.length != 2) {
+                console.error(`Train ${train.trainNumber} has ${train.timeTableRows.length} interesting stops.`);
+                console.log(JSON.stringify(train));
+            }
+            // From and to stations should be in travel order, according to API specification.
+            else if (train.timeTableRows[0].stationShortCode != fromStationCode || train.timeTableRows[1].stationShortCode != toStationCode) {
+                console.error(`Train ${train.trainNumber} has interesting stops in unexpected order.`);
+                console.log(JSON.stringify(train));
+            }
+            else {
+                renderTableRow(train, trainListElement);
+            }
+        });
+    });
+}
+
+main();
